3c15dcc3c772b0347d57ce5d99aacabc317dfdf0
Tumelo850 <User@DESKTOP-LV13BPA>
2025-03-03 18:49:34 +0200

    Add project files.

---
+++ TumeloStudyGroup/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TumeloStudyGroup.MvcApplication" Language="C#" %>
---
+++ TumeloStudyGroup/Models/ST_Table.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace TumeloStudyGroup.Models
+{
+    public class ST_Table
+    {
+        [DisplayNameAttribute("STUDENT NUMBER")]
+        public string St_Number { get; set; }
+        [DisplayNameAttribute(" NAME")]
+        public string St_Name { get; set; }
+        [DisplayNameAttribute("SURNAME")]
+        public string St_Surname { get; set; }
+        [DisplayNameAttribute("EMAIL ADDRESS")]
+        public string St_Email { get; set; }
+    }
+}
